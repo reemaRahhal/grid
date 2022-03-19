7c0bad7fe02d336ec79057ea8f2161400d44591c
reema rahhal <reema.rahhal96@gmail.com>
2022-03-19 20:27:40 +0200

    just add file to project

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var firstName = prompt("please enter your name") ;
+alert("Welcoe "+ firstName + " to our website, Enjoy!");
+
+function msg2(){
+    alert("“We can do no great things, only small things with great love.”"+"\n"+" Mother Teresa");
+}  
+function msg6(){
+    alert("“If you’re feeling helpless, help someone.”"+"\n"+" Aung San Suu Kyi");
+} 
+function msg8(){
+    alert("“People will forget what you said, people will forget what you did, but people will never forget how you made them feel.” "+"\n"+"Maya Angelou");
+}  
+function msg12(){
+alert("“The most effective way to do it, is to do it.” — Amelia Earhart");
+} 
+function msg18(){
+    alert("“If we have no peace, it is because we have forgotten that we belong to each other.” — Mother Teresa");
+} 
+function msg15(){
+    alert("“No one has ever become poor by giving.” — Anne Frank");
+}  
+ 
